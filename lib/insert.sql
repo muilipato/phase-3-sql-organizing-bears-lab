9307d3bb17ec00efba1daf087a9522aea7a4f953
Patrick Maweu <60652548+muilipato@users.noreply.github.com>
2023-06-02 17:58:32 +0300

    done

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 3, "M", "black and white", "playful", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 1, "M", "black", "pugnacious", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 5, "F", "white", "silly", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 8, "M", "brown", "playful", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 2, "F", "black", "aggressive", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 3, "M", "brown", "ornery", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 1, "F", "white", "calm", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 6, "F", "black", "sleepy", 1);
